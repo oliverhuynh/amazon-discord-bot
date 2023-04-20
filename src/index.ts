b64db2838fdd2eb27815b2a2d6fd92a9549807fe
Oliver Huynh <oliver@jufist.com>
2023-04-20 18:12:20 +0700

    AmazonSite

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { scrapeAmazonSearch } from './scrapeAmazonSearch';
+import { AmazonSite, Product } from './types';
 require('dotenv').config();
-executablePath = process.env.CHROME_EXECUTABLE_PATH;
+const domains:string = process.env.DOMAINS;
 
-async function run() {
+const run = async () => {
   while (true) {
     try {
       const domains = process.env.DOMAINS;
-      const results = await scrapeAmazonSearch('', domains.split(','));
+      const results = await scrapeAmazonSearch('', domains.split(',') as AmazonSite[]);
       console.log(results);
     } catch (error) {
       console.log(`Error running scrapeAmazonSearch: ${error.message}`);
