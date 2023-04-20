5c3fa904ab69a1181432ac06c59c03fdf0175a09
Oliver Huynh <oliver@jufist.com>
2023-04-20 17:36:13 +0700

    BotForever

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { scrapeAmazonSearch } from './scrapeAmazonSearch';
 
 async function run() {
-  const results = await scrapeAmazonSearch('', ['amazon.es', 'amazon.fr', 'amazon.com']);
-  console.log(results);
+  while (true) {
+    try {
+      const results = await scrapeAmazonSearch('', ['amazon.es', 'amazon.fr', 'amazon.com']);
+      console.log(results);
+    } catch (error) {
+      console.log(`Error running scrapeAmazonSearch: ${error.message}`);
+    }
+  }
 }
 
 run();
