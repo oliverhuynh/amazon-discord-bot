77d7b54242d3ec250e99ea3c666c0c6ab06471d3
Oliver Huynh <oliver@jufist.com>
2023-04-20 18:03:02 +0700

    DomainsNowConfigurable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { scrapeAmazonSearch } from './scrapeAmazonSearch';
+require('dotenv').config();
+executablePath = process.env.CHROME_EXECUTABLE_PATH;
 
 async function run() {
   while (true) {
     try {
-      const results = await scrapeAmazonSearch('', ['amazon.es', 'amazon.fr', 'amazon.com']);
+      const domains = process.env.DOMAINS;
+      const results = await scrapeAmazonSearch('', domains.split(','));
       console.log(results);
     } catch (error) {
       console.log(`Error running scrapeAmazonSearch: ${error.message}`);
